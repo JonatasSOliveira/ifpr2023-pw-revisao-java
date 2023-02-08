4f9c51e9ddb080bcb7a50bb401fcf54f97e9ab45
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-02-07 21:43:42 -0300

    Finalizando resolução segundo exercício

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            String[] lista = DialogUtils.solicitarListaStrings("Informe a lista de valores:");
+            String[] lista = DialogUtils.solicitarListaStrings("Informe a lista de valores separados por \",\":");
             String mensagem = "Lista original: " + ListUtils.juntarLista(lista)
                     + "\nLista inversa: " + ListUtils.juntarLista(ListUtils.inverterLista(lista));
             DialogUtils.exibirMensagem(mensagem);
